a4d9a5cea3475d163479913891f27ee99450f680
Mai~ <roekhan.dani.maulana@students.untidar.ac.id>
2024-09-02 20:37:04 +0700

    Add Assignment folder

---
+++ PBO_BekerjaDenganJava/MangmaiProject1/src/main/java/com/lemai/mangmaiproject1/MangmaiProject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lemai.mangmaiproject1;
+
+/**
+ *
+ * @author Mai
+ */
+public class MangmaiProject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Name    : Mai Aphrodite");
+        System.out.println("NPM     : 9201752890");
+        System.out.println("Addr    : Ohio 69");
+        System.out.println("Phone   : 69420666911");
+    }
+}
