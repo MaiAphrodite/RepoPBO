dd84cadfc1e3de5b9fba06c00d66c99d15d4b563
Mai~ <roekhan.dani.maulana@students.untidar.ac.id>
2024-10-22 08:39:52 +0700

    meng!

---
+++ Pertemuan_9/Mai_P9/mavenproject4/src/main/java/com/lemai/mamaip9/Kucing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.lemai.mamaip9;
+
+/**
+ *
+ * @author ASUS
+ */
+class Kucing extends Hewan {
+    @Override
+    public void suara() {
+        System.out.println("Kucing: Meong!");
+    }
+}
